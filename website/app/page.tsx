98a0fb25b57a27458df2b269207345d8f4766095
Henri <98414850+i-am-henri@users.noreply.github.com>
2023-12-21 12:53:37 +0000

    create a link

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import {motion} from  "framer-motion"
 import { Metadata } from "next"
 export const metadata: Metadata = {
@@ -8,7 +9,7 @@ export default function Home() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#FFFFF0]">
       <h2  className="text-black text-4xl">I am henri.</h2>
       <p className="text-black">I code weird stuff on the internet.</p>
-      
+      <Link className="text-black underline" href="https://github.com/i-am-henri">GitHub</Link>
     </div>
   )
 }
